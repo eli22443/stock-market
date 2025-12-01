e0a3c08dcabfb5d6262cd39b7bdf76630e550590
eli22443 <eli22443@gmail.com>
2025-12-01 13:54:36 +0200

    refactor

--- frontend/app/stocks/layout.tsx
+++ frontend/app/stocks/layout.tsx
@@ -1,4 +1,15 @@
 import StocksMenu from "@/components/StocksMenu";
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default function StocksLayout({
   children,
@@ -10,6 +21,8 @@ export default function StocksLayout({
     <div>
       <h1 className="mb-6 font-bold text-2xl">Stocks</h1>
       <StocksMenu />
+      <Input className="w-20" type="text" />
+      <Table className="" />
       {children}
     </div>
   );
