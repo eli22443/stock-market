9a04cf2d2bdb19abce76924ba8c9f3e01848fb31
eli22443 <eli22443@gmail.com>
2025-12-07 19:53:51 +0200

    modified stock cards

--- frontend/components/StockCard.tsx
+++ frontend/components/StockCard.tsx
@@ -3,29 +3,43 @@ import type { StockRecord } from "@/types";
 
 export default function StockCard({ stock }: { stock: StockRecord }) {
   return (
-    <div className=" rounded-2xl border-2 border-blue-800 px-2 py-2">
+    <div className="w-60 rounded-2xl border-2 border-blue-800 px-2 py-4">
       <div className="stock-info">
-        <h1 className="text-2xl font-bold font-mono ">{stock.symbol}</h1>
-        <p>
-          Current Price: <strong>${stock.data.c}</strong>
-        </p>
-        <p>
-          High Price of the day: <strong>${stock.data.h}</strong>
-        </p>
-        <p>
-          Low Price of the day: <strong>${stock.data.l}</strong>
-        </p>
-        <p>
-          Open Price of the day: <strong>${stock.data.o}</strong>
-        </p>
-        <p>
-          Previous Close Price: <strong>${stock.data.pc}</strong>
-        </p>
+        <div className="flex justify-center my-4">
+          <h1 className="text-2xl font-bold font-mono mr-6">{stock.symbol}</h1>
+          <span className="text-2xl font-bold font-mono">${stock.data.c}</span>
+        </div>
+        <div className="mx-2">
+          <div className="flex justify-between">
+            <span>High:</span>
+            <span>
+              <strong>${stock.data.h}</strong>
+            </span>
+          </div>
+          <div className="flex justify-between">
+            <span>Low:</span>
+            <span>
+              <strong>${stock.data.l}</strong>
+            </span>
+          </div>
+          <div className="flex justify-between">
+            <span>Open:</span>
+            <span>
+              <strong>${stock.data.o.toFixed(2)}</strong>
+            </span>
+          </div>
+          <div className="flex justify-between">
+            <span>Close:</span>
+            <span>
+              <strong>${stock.data.pc}</strong>
+            </span>
+          </div>
+        </div>
       </div>
       <div className="stock-graph py-4">
         {/* Placeholder for stock chart - to be implemented */}
       </div>
-      <div className="stock-overlay mb-2">
+      <div className="stock-overlay mb-2 ml-6">
         <Link
           href={`/quote/${stock.symbol}`}
           className="px-4 py-2 bg-blue-700 text-white rounded hover:bg-blue-800"
